26957c02aafdd3ac1a2545a073570b2eb12e736e
FlowerKei <gustkd1990@gmail.com>
2018-02-20 08:17:04 +0900

    Update fabcar.go

--- chaincode/fabcar/fabcar.go
+++ chaincode/fabcar/fabcar.go
@@ -29,7 +29,7 @@ package main
 
 /* Imports
  * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
- * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+ * 2 Hyperledger Fabric specific libraries for Smart Contracts
  */
 import (
 	"bytes"
