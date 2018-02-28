695a354fb670ab4418a9fdee427b39619fc9175a
FlowerKei <gustkd1990@gmail.com>
2018-02-28 10:03:06 +0900

    Update enrollAdmin.js

--- fabcar/enrollAdmin.js
+++ fabcar/enrollAdmin.js
@@ -29,7 +29,7 @@ var member_user = null;
 var store_path = path.join(__dirname, 'hfc-key-store');
 console.log(' Store path:'+store_path);
 
-
+// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 Fabric_Client.newDefaultKeyValueStore({ path: store_path
 }).then((state_store) => { // .then(function (state_store)){
     // assign the store to the fabric client
